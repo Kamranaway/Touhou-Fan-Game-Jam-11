362eab161b279344ed3b8df358bd81688a5cf500
Kamranaway <54047955+Kamranaway@users.noreply.github.com>
2023-09-02 01:56:49 -0700

    Hints can gray out

--- assets/scripts/Hint.gd
+++ assets/scripts/Hint.gd
@@ -12,6 +12,10 @@ func set_text_visible(is_visible: bool):
 	$CenterContainer/HintLabel.visible = is_visible
 
 func grey_out(is_grey: bool):
-	#if is_grey:
-		#self.modulate
-	pass
+	if is_grey:
+		self.modulate = Color.DARK_GRAY
+	else:
+		self.modulate = Color.WHITE
+
+func _ready():
+	grey_out(true)
